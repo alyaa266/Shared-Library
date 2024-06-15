db2b66348c49f6e6c5e1e119c8f37e988c973b18
alyaa266 <84872262+alyaa266@users.noreply.github.com>
2024-06-15 18:03:49 +0300

    Update SonarQubeTest.groovy

--- vars/SonarQubeTest.groovy
+++ vars/SonarQubeTest.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(){ 
-	withSonarQubeEnv(sq)
-  {
-     Sh "./gradlew sonarqube"
-  }
+	withSonarQubeEnv(installationName: 'sq' ) { 
+	
+		sh"./gradlew sonarqube"
+	 
+	}
+
 }

e0129a0d83b7b70fa35b81abdf4e423dbb5a8ec0
alyaa266 <84872262+alyaa266@users.noreply.github.com>
2024-06-15 17:54:15 +0300

    Update SonarQubeTest.groovy

--- vars/SonarQubeTest.groovy
+++ vars/SonarQubeTest.groovy
@@ -1,9 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(){ 
-	withSonarQubeEnv(credentialsId: 'sonarqube',installationName: 'sq' ) { 
-	
-		sh"./gradlew sonarqube   -Dsonar.projectKey=App-spring  -Dsonar.host.url=http://localhost:9000 "
-	 
-	}
+	withSonarQubeEnv(sq)
+  {
+     Sh ‘./gradlew sonarqube’
+  }
 }
